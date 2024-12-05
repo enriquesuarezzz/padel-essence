054c3f2117e203c7226815a435aa2e94d1f167d4
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-12-05 16:52:45 +0000

    i18nDictAdded

---
+++ src/app/[lang]/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata, ResolvingMetadata } from 'next'
+import { GeneralProps } from '@/interfaces/lang-props'
+import { getDictionary } from './dictionaries'
+
+export async function generateMetadata(
+  { params: { lang } }: GeneralProps,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const dict = await getDictionary(lang)
+
+  return {
+    title: `${dict.metadata.title}`,
+    description: `${dict.metadata.description}`,
+  }
+}
+export default async function Home({ params: { lang } }: GeneralProps) {
+  const dict = await getDictionary(lang)
+  return <main></main>
+}
