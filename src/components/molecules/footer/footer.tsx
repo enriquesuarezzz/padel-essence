1a6c087e771853d9730d18b693ad944249f88b5d
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2025-01-16 19:46:47 +0000

    footerMinorUpdate

--- src/components/molecules/footer/footer.tsx
+++ src/components/molecules/footer/footer.tsx
@@ -7,7 +7,7 @@ import X from '@/components/atoms/svg/x'
 import Link from 'next/link'
 export default function Footer({ dict }: LangProps) {
   return (
-    <footer className="bottom-0 flex w-full flex-col items-center justify-items-center bg-[#1e1e1e] py-4 md:mt-24 md:py-10">
+    <footer className="flex w-full flex-col items-center justify-items-center bg-[#1e1e1e] py-4 md:mt-24 md:py-10">
       <Image src="/images/logo.avif" alt="logo" width={100} height={100} />
       <div className="flex flex-col items-center gap-8">
         <div className="flex gap-1 px-3 md:gap-6">
